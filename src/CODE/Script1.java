32d5b39d2a173e463616ab64bdad8e0c7c5dcfc7
chauhanamit4211 <poadm.isv@gmail.com>
2015-08-31 17:21:18 +0530

    Code Received by chauhanamit4211
    This is code received again by chauhanamit4211 as there was some issue
    with code received earlier.

--- src/CODE/Script1.java
+++ src/CODE/Script1.java
@@ -6,5 +6,6 @@ public class Script1 {
 		System.out.println("Script created from local machine of amitchauhan4211");
 		System.out.println("chauhanamit4211 received code from public repository.");
 		System.out.println("Hello Mr. chauhanamit4211, have you received code successfully!");
+		System.out.println("Yes! i received it. Thanks Mr. amitchauhan4211.");
 	}
 }
