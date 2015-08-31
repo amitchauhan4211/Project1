628f379d25054d5991ad5d90b99d142e252377e8
amitchauhan4211 <sysadm.isv@gmail.com>
2015-08-31 16:46:22 +0530

    ask about received code from user chauhanamit4211
    Verify does commit from GitHub desktop client is enough for code push to
    Public repository on web?

--- src/CODE/Script1.java
+++ src/CODE/Script1.java
@@ -5,5 +5,6 @@ public class Script1 {
 	public static void main(String[] args) {
 		System.out.println("Script created from local machine of amitchauhan4211");
 		System.out.println("chauhanamit4211 received code from public repository.");
+		System.out.println("Hello Mr. chauhanamit4211, have you received code successfully!");
 	}
 }
