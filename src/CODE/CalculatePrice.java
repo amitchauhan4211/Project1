b58dc7b52599e076eb38177c18ce1f68c1f3fadd
chauhanamit4211 <poadm.isv@gmail.com>
2015-09-29 15:16:39 +0530

    Web font pageviews counter
    Adding 1  more file that will calculate price as per web fonts page
    views.

---
+++ src/CODE/CalculatePrice.java
@@ -0,0 +1,13 @@
+package CODE;
+
+public class CalculatePrice {
+	public static void main(String[] args) {
+		  
+		  int webPageViews=250000, amount=35;
+		  for(int i=1;i<=70;i++)
+		  {
+		   System.out.println("PageViews : "+webPageViews+"   Cost : $"+amount*i+"    times: "+i +" X");
+		   webPageViews=webPageViews+750000; 
+		  }
+	 }
+}
