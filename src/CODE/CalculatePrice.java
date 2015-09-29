8af123e8c2ff8681573e5fba86d9774fbf385c31
chauhanamit4211 <poadm.isv@gmail.com>
2015-09-29 15:49:45 +0530

    Revert "Revert "Loop Comments Added""
    This reverts commit 426e0bbcba133fb44df97c37c0b998380d59ab3c.

--- src/CODE/CalculatePrice.java
+++ src/CODE/CalculatePrice.java
@@ -4,6 +4,7 @@ public class CalculatePrice {
 	public static void main(String[] args) {
 		  
 		  int webPageViews=250000, amount=35;
+		  //loop will calculate pageviews and then price for those page views.
 		  for(int i=1;i<=70;i++)
 		  {
 		   System.out.println("PageViews : "+webPageViews+"   Cost : $"+amount*i+"    times: "+i +" X");
