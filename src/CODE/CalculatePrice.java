fc3d1996e29c4cb34e5ee4b284088c211f11af95
chauhanamit4211 <poadm.isv@gmail.com>
2015-09-29 15:42:09 +0530

    Comments Removed.

--- src/CODE/CalculatePrice.java
+++ src/CODE/CalculatePrice.java
@@ -9,6 +9,5 @@ public class CalculatePrice {
 		   System.out.println("PageViews : "+webPageViews+"   Cost : $"+amount*i+"    times: "+i +" X");
 		   webPageViews=webPageViews+750000; 
 		  }
-		  System.out.println("Working");
 	 }
 }
